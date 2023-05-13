8cc8d5af5052c21a89d34c21e7ff3f45e8b13315
Filip Hoang <2hoang7@gmail.com>
2023-05-13 16:32:01 +0200

    Allow sending custom response bodies, headers and content types along with the Status codes

--- app.js
+++ app.js
@@ -4,12 +4,22 @@ const port = process.env.PORT || 8080;
 
 app.get('/status/:statusCode', (req, res) => {
   const statusCode = parseInt(req.params.statusCode, 10);
-  if (isNaN(statusCode) || statusCode < 100 || statusCode >= 600) {
-    res.status(400).send('Invalid status code. Please provide a valid HTTP status code between 100 and 599.');
-  } else {
-    res.status(statusCode).send(`Status code ${statusCode} returned.`);
+  const responseBody = req.query.body || '';
+  const contentType = req.query.contentType || 'text/plain';
+  const customHeaders = req.query.headers ? JSON.parse(req.query.headers) : {};
+
+  // Set content type
+  res.setHeader('Content-Type', contentType);
+
+  // Set custom headers
+  for (const [key, value] of Object.entries(customHeaders)) {
+    res.setHeader(key, value);
   }
+
+  // Send status code and custom response body
+  res.status(statusCode).send(responseBody);
 });
+
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
