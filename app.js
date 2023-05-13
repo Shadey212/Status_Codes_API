d5e120c099e3b1613ec06c70612199779c66d7f4
Filip <75810145+Shadey212@users.noreply.github.com>
2023-05-13 15:36:28 +0200

    Initial Commit
    First commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/status/:statusCode', (req, res) => {
+  const statusCode = parseInt(req.params.statusCode, 10);
+  if (isNaN(statusCode) || statusCode < 100 || statusCode >= 600) {
+    res.status(400).send('Invalid status code. Please provide a valid HTTP status code between 100 and 599.');
+  } else {
+    res.status(statusCode).send(`Status code ${statusCode} returned.`);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
