8093013216946d8489bde52b9af184e92b8da614
Filip Hoang <2hoang7@gmail.com>
2023-05-13 15:52:32 +0200

    Bind to Heroku-assigned port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.get('/status/:statusCode', (req, res) => {
   const statusCode = parseInt(req.params.statusCode, 10);
