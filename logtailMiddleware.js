98bf65a2f1c7a98c66c721a15d5588303c8c6e95
Filip Hoang <2hoang7@gmail.com>
2023-05-13 17:48:07 +0200

    Fixed Logtail middlewar duplicate

--- logtailMiddleware.js
+++ logtailMiddleware.js
@@ -4,25 +4,22 @@ const logtail = new Logtail(process.env.LOGTAIL_SOURCE_TOKEN);
 const logtailMiddleware = (req, res, next) => {
   const startTime = Date.now();
 
-  const logtailMiddleware = (req, res, next) => {
-    const startTime = Date.now();
-  
-    res.on('finish', () => {
-      const responseTime = Date.now() - startTime;
-      const headers = Object.entries(req.headers).map(([key, value]) => ({ key, value })); // Convert headers to array of objects
-  
-      logtail.info({
-        method: req.method,
-        path: req.path,
-        status: res.statusCode,
-        duration: responseTime,
-        ip: req.ip,
-        headers,
-      });
+  res.on('finish', () => {
+    const responseTime = Date.now() - startTime;
+    const headers = Object.entries(req.headers).map(([key, value]) => ({ key, value })); // Convert headers to array of objects
+
+    logtail.info({
+      method: req.method,
+      path: req.path,
+      status: res.statusCode,
+      duration: responseTime,
+      ip: req.ip,
+      headers,
     });
-  
-    next();
-  };
+  });
+
+  next();
+};
 
 process.on('uncaughtException', (error) => {
   logtail.error('Uncaught exception', { error });
